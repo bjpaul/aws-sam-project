8af9275dc43f5c7acd58cb472837ed2c4660c939
Bijoy Paul <work.bijoypaul@gmail.com>
2020-12-30 01:09:51 +0530

    script added

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -xe
-git diff-tree --no-commit-id --name-only -r $CODEBUILD_RESOLVED_SOURCE_VERSION
+git diff-tree --no-commit-id --name-only -r $CODEBUILD_RESOLVED_SOURCE_VERSION > output.txt
