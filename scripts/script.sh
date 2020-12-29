ac2f0c39166a34195af8934c92dbcbcdafb72a85
Bijoy Paul <work.bijoypaul@gmail.com>
2020-12-30 01:16:47 +0530

    refactored

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -xe
-git diff-tree --no-commit-id --name-only -r $CODEBUILD_RESOLVED_SOURCE_VERSION > output.txt
+git diff --name-only
+#git diff-tree --no-commit-id --name-only -r $CODEBUILD_RESOLVED_SOURCE_VERSION
